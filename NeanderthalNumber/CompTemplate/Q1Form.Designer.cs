f79531ed25e8908ba4484538cf3b847278543aad
loganschool <47428384+loganschool@users.noreply.github.com>
2019-03-21 20:34:00 -0400

    Added all files

---
+++ NeanderthalNumber/CompTemplate/Q1Form.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CompTemplate
+{
+    partial class Q1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(338, 30);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 27);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbxOutput.Location = new System.Drawing.Point(12, 63);
+            this.tbxOutput.Multiline = true;
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.ReadOnly = true;
+            this.tbxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxOutput.Size = new System.Drawing.Size(422, 392);
+            this.tbxOutput.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(447, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.mniOpen_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(13, 28);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(233, 25);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Waiting on a data file...";
+            // 
+            // Q1Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 495);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.tbxOutput);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Q1Form";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ NeanderthalNumber/CompTemplate/Q1Form.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CompTemplate
+{
+    public partial class Q1Form : Form
+    {
+        // Variable representing the path of the data file chosen by user
+        public string path;
+
+        // Neandrethal words, divided into 2 lists for less items to iterate through
+        string[] o_words = { "ook", "ookook", "oog", "ooga", "oogam", "oogum" };
+        string[] um_words = { "mook", "mookmook", "ug", "ugug" };
+
+        // Hashtable to store all previously calculated strings
+        Hashtable previouslyCalculatedStrings = new Hashtable();
+
+        public Q1Form()
+        {
+            InitializeComponent();
+        }
+
+        // Start button pressed, starting to solve the problem
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartSolution();
+        }
+
+        // Open FileDialog to allow user to choose their input file
+        private void mniOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog datafileSelect = new OpenFileDialog();
+
+            if (datafileSelect.ShowDialog() == DialogResult.OK)
+            {
+                path = datafileSelect.FileName;
+            }
+
+            btnStart.Enabled = true;
+            lblStatus.Text = "Data file loaded. Press start...";
+        }
+
+        // Function to output on the screen. If you set nl to false, it will not add a new line after the thing you're printing.
+        public void write(string text, bool nl = true)
+        {
+            tbxOutput.AppendText(text + (nl ? Environment.NewLine : ""));
+        }
+
+        // Main recursive function. Takes a string s to be searched.
+        int neanNumbers(string s)
+        {
+            // Variable that tells you how many possible numbers can be
+            // created with the current substring s
+            int poss = 0;
+
+            // Base Case: no characters left means a successful collection of numbers
+            if (s.Length == 0)
+            {
+                return 1;               
+            }
+
+            // Dynamic programming: Check if this string has already been calculated,
+            // in which case set poss to be the previously calculated value
+            else if (previouslyCalculatedStrings.Contains(s))
+            {
+                return (int) previouslyCalculatedStrings[s];
+            }
+
+            // Not base case or previously calculated
+            else
+            {
+                // Contains a list where only the relevant words exist
+                string[] wordlist;
+
+                // If the first letter is o, only search through words that start with o
+                if (s[0] == 'o')
+                {
+                    wordlist = o_words;
+                }
+                // Otherwise, search all other words
+                else
+                {
+                    wordlist = um_words;
+                }
+        
+                // For every word in wordlist, see if that word is a possible new path
+                foreach (string word in wordlist)
+                {
+                    bool equal = true;
+
+                    // Making sure that the current word isn't longer than the current
+                    // string. If it is, then obviously it isn't a possible path
+                    if (word.Length <= s.Length)
+                    {
+                        // Iterating through each character in word and string
+                        // To see if they are the same
+                        for (int i = 0; i < word.Length; i++)
+                        {  
+                            if (s[i] != word[i])
+                            {
+                                // Word doesn't equal the string. set equal to false and break from the loop.
+                                equal = false;
+                                break;
+                            }
+                        }
+
+                        // If the string and word are equal, figure out how many possibilities there are
+                        // In a substring that doesn't contain the current word.
+                        if (equal)
+                        {
+                            poss += neanNumbers(s.Substring(word.Length));                           
+                        }
+                    }    
+                }
+            }
+            
+            // We have calculated the value of this string, so we can add it to the
+            // hastable of previously calculated strings for future reference
+            previouslyCalculatedStrings.Add(s, poss);
+            
+            // Return the number of possibilities in the current substring.
+            return poss;
+        }
+
+        // Function to be called when it is time to solve the problem
+        public void StartSolution()
+        {
+            // Read all of the input data and store it in the list data
+            List<string> data = new List<string>(System.IO.File.ReadAllLines(path));
+
+            // Loop for iterating through cases
+            for (int CaseNum = 0; CaseNum < 10; CaseNum++)
+            {
+                // Problem contains the currrent neandrethal number to be calculated
+                string problem = data[CaseNum];
+
+                // Calculate and write how many possibilities there are in the current problem
+                int possibilities = neanNumbers(problem);
+                write(Convert.ToString(possibilities));
+            }       
+        }
+    }
+}
